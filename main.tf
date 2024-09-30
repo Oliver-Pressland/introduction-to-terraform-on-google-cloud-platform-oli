054d2e67dd883cd4da96735b5c027f050bbf55ef
Oliver-Pressland <56721245+Oliver-Pressland@users.noreply.github.com>
2024-09-30 22:25:23 +0100

    Revert back to original

--- main.tf
+++ main.tf
@@ -1,22 +1,10 @@
-resource "google_compute_network" "app" {
-  name                    = "app"
-  auto_create_subnetworks = false
-}
-
-resource "google_compute_subnetwork" "app" {
-  name          = "app"
-  ip_cidr_range = "10.2.0.0/16"
-  region        = "us-west1"
-  network       = google_compute_network.app.id
-}
-
 data "google_compute_image" "ubuntu" {
   most_recent = true
   project     = "ubuntu-os-cloud" 
   family      = "ubuntu-2204-lts"
 }
 
-resource "google_compute_instance" "app" {
+resource "google_compute_instance" "web" {
   name         = "web"
   machine_type = "e2-micro"
 
@@ -27,7 +15,7 @@ resource "google_compute_instance" "app" {
     }
   }
   network_interface {
-   subnetwork = "app"
+   subnetwork = "default"
    access_config {
       # Leave empty for dynamic public IP
     }
