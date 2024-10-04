c0c7e236189c86e07eeea88a20a7aaa9bcf5c409
Oliver-Pressland <56721245+Oliver-Pressland@users.noreply.github.com>
2024-10-04 18:29:32 +0100

    Added new network

--- main.tf
+++ main.tf
@@ -1,6 +1,37 @@
+module "app_network" {
+  source  = "terraform-google-modules/network/google"
+  version = "9.2.0"
+  
+  network_name = "${var.network_name}-network"
+  project_id   = var.project_id
+  subnets = [
+    {
+      subnet_name   = "${var.network_name}-subnet-01"
+      subnet_ip     = "10.10.10.0/24"
+      subnet_region = "us-west1"
+    }
+  ]
+
+  ingress_rules = [
+    {
+      name                    = "${var.network_name}-web"
+      description             = "Inbound web"
+
+      source_ranges           = ["0.0.0.0/0"]
+      target_tags             = ["${var.network_name}-web"]
+
+      allow = [
+        {
+          protocol = "tcp"
+          ports    = ["80","443"]
+        }
+      ]
+    }
+  ]
+}
+
 resource "google_compute_network" "app" {
   name                    = var.network_name
-  auto_create_subnetworks = false
 }
 
 resource "google_compute_subnetwork" "app" {
@@ -27,7 +58,7 @@ resource "google_compute_instance" "web" {
     }
   }
   network_interface {
-   subnetwork = google_compute_subnetwork.app.name
+   subnetwork = module.app_network.subnets_names[0]
    access_config {
       # Leave empty for dynamic public IP
     }
